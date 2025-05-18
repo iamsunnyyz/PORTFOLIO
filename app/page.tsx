9210dd92fa92220a0cb46cd77a7468d2e1048e37
iamsunnyyz <sunny170600@gmail.com>
2025-05-18 22:52:18 +0530

    latest fix of document error

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import IntroSection from '@/components/IntroSection'
-import ExperienceSection from '@/components/ExperienceSection'
-import Contact from '@/components/Contact'
-import Skills from '@/components/Skills'
-import SpotlightEffect from '@/components/SpotlightEffect'
-import About from '@/components/About'
-import Projects from '@/components/Projects'
+'use client';
+import dynamic from 'next/dynamic'
 
+// Server components
+import Footer from '../components/Footer'
+import Contact from '@/components/Contact'
+
+// Client components with dynamic imports
+const Navbar = dynamic(() => import('../components/Navbar'), { ssr: false })
+const IntroSection = dynamic(() => import('@/components/IntroSection'), { ssr: false })
+const ExperienceSection = dynamic(() => import('@/components/ExperienceSection'), { ssr: false })
+const Skills = dynamic(() => import('@/components/Skills'), { ssr: false })
+const SpotlightEffect = dynamic(() => import('@/components/SpotlightEffect'), { ssr: false })
+const About = dynamic(() => import('@/components/About'), { ssr: false })
+const Projects = dynamic(() => import('@/components/Projects'), { ssr: false })
 
 export default function Home() {
   return (
